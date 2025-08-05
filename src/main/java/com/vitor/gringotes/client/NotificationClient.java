ddd4b662bc932858a905d56a421db19688e1fdf3
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-08-05 18:42:46 -0300

    Serviço de consulta ao autorizador externo criado e serviço que irá notificar o recebimento de transferencia criado

---
+++ src/main/java/com/vitor/gringotes/client/NotificationClient.java
@@ -0,0 +1,19 @@
+package com.vitor.gringotes.client;
+
+
+import com.vitor.gringotes.entity.Transfer;
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        url = "${client.notification-service.url}"
+)
+public interface NotificationClient {
+
+
+    @PostMapping
+    ResponseEntity<Void> sendNotification(@RequestBody Transfer transfer);
+}
